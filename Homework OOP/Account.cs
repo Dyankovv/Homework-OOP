976491e97692a3b36aca3c429bccbb76db052585
prisi <prisi@DESKTOP-7H0JI5E>
2023-12-18 12:32:57 +0200

    Add project files.

---
+++ Homework OOP/Account.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_OOP
+{
+    public class Account
+    {
+        private int userID;
+        private double userBalance;
+        public Account(int userid, double userbalance)
+        {
+            ID = userid;
+            Balance = userbalance;
+        }
+        public int ID
+        {
+            get { return userID; }
+            set
+            {
+                if (value > 0) userID = value;
+                else throw new Exception("ID can't be negaitve or zero");
+            }
+        }
+        public double Balance
+        {
+            get { return userBalance; }
+            set
+            {
+                if (value >= 0) userBalance = value;
+                else throw new Exception("Balance can't be negative");
+            }
+        }
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+        public void Withdraw(double amount)
+        {
+            if (Balance - amount < 0)
+            {
+                throw new Exception("Insufficient balance");
+            }
+            else Balance -= amount;
+            
+        }
+        public override string ToString()
+        {
+            return ($"Account ID{ID}, Balance {Balance:F2} ");
+        }
+
+    }
+}
---
+++ Homework OOP/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework_OOP
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Dictionary<int, Account> accounts = new Dictionary<int, Account>();
+
+            string command = Console.ReadLine();
+            while (command != "End")
+            {
+                string[] commandArguments = command.Split(' ');
+                int currentAccount = int.Parse(commandArguments[1]);
+                if (commandArguments[0] == "Create")
+                {
+                    if (accounts.ContainsKey(currentAccount))
+                    {
+                        Console.WriteLine("Account already exists");
+                    }
+                    else accounts.Add(currentAccount, new Account(currentAccount, 0));                  
+                }
+                else if (commandArguments[0] == "Deposit")
+                {
+                    double depositValue = double.Parse(commandArguments[2]);
+                    if (accounts.ContainsKey(currentAccount))
+                    {
+                        accounts[currentAccount].Deposit(depositValue);
+                    }
+                    else Console.WriteLine("Account does not exist");
+                }
+                else if (commandArguments[0] == "Withdraw")
+                {
+                    double withdrawValue = double.Parse(commandArguments[2]);
+                    if (accounts.ContainsKey(currentAccount))
+                    {
+                        try
+                        {
+                            accounts[currentAccount].Withdraw(withdrawValue);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                    else Console.WriteLine("Account does not exist");
+                }
+                else if (commandArguments[0] == "Print")
+                {
+                    if (accounts.ContainsKey(currentAccount))
+                    {
+                        Console.WriteLine(accounts[currentAccount].ToString());
+                    }
+                    else Console.WriteLine("Account does not exist");
+                }
+                command = Console.ReadLine();
+            }
+
+        }
+    }
+}
